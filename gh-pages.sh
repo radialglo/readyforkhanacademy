6c0b01635a14281debd042172cd16faa08959848
Anthony  Su <antchovee@gmail.com>
2014-08-29 15:26:55 -0700

    * updated gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-
 GH=gh-pages
 M=master
 OUTDIR="."
@@ -12,13 +9,14 @@ if [ ! -d "node_modules" ]; then
 fi
 
 ./node_modules/.bin/jade -o $OUTDIR views/index.jade
-# If -B is given, <new_branch> is created if it doesn’t exist; otherwise, it is reset. This is the transactional equivalent of
+# If -B is given, <new_branch> is created if it doesn’t exist; otherwise, it is reset. This is the transactional equivale#
 
 # $ git branch -f <branch> [<start point>]
 # $ git checkout <branch>
 # that is to say, the branch is not reset/created unless "git checkout" is successful.
 git checkout -B $GH
-# git merge $M 
+# git merge $M.
+git merge -s subtree master
 rm -rf Procfile app.js package.json Gruntfile.js README.md node_modules views
 mv assets/* .
 rm -rf scss
